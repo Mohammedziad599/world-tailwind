2cb84b3c7013ec2d4b0719a7e13f87df7866df02
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-07-06 11:08:34 +0300

    implement the search and getting data from an api

---
+++ js/list.js
@@ -0,0 +1,134 @@
+let searchTypingTimer;
+
+let showingAllCountries = true;
+
+const searchInput = document.querySelector("#search");
+
+searchInput.addEventListener('keyup', (event) => {
+  clearTimeout(searchTypingTimer);
+  let searchValue = event.currentTarget.value.trim();
+  searchTypingTimer = setTimeout(() => {
+    if (searchValue.length === 0) {
+      if (!showingAllCountries) {
+        getAllCountries();
+      }
+      return;
+    }
+    createGridSkeleton();
+    fetch(`https://restcountries.com/v3.1/name/${searchValue}?fields=name,cca3,region,population,capital,flags`)
+    .then(response=>response.json())
+    .then(data=> {
+      showCountries(data);
+      showingAllCountries = false;
+    })
+    .catch(error=>console.error(error));
+  }, 400);
+});
+
+/**
+ *
+ * @param {*} data
+ */
+ function showCountries(data) {
+  if(Array.isArray(data)){
+    let gridInner = '';
+    data.forEach((element, index, array) => {
+      let flag = element.flags.svg;
+      let name = element.name.common;
+      let population = element.population;
+      let region = element.region;
+      let capital = element.capital[0] || "No Capital";
+      let code = element.cca3;
+      let template = `
+        <div class="mb-10">
+          <a href="./country.html?code=${code}" class="card block bg-white text-black dark:bg-gray-800 rounded-lg shadow-lg
+          dark:text-white overflow-hidden hover:transition-all ease-in-out duration-200 hover:scale-105">
+            <img src="${flag}" alt="${name + " Flag"}" class="w-full h-fit lg:h-72 xl:h-48 object-cover">
+            <div class="card-body px-5 pt-5 pb-10">
+              <h3 class="text-xl font-semibold mb-4 with-popup hidden xl:block" aria-hidden="true"
+                ${name.length > 25 ? 'data-toggle="popover" data-content="' + name + '"' : ""}>
+                ${name.length > 25 ? name.substr(0, 24) + "&hellip;" : name}
+              </h3>
+              <h3 class="text-xl font-semibold mb-4 with-popup block xl:hidden xl:sr-only">
+                ${name}
+              </h3>
+              <div class="mb-1">
+                <span class="font-semibold mr-1">
+                  Population:
+                </span>
+                ${population.toLocaleString('en-US')}
+              </div>
+              <div class="mb-1">
+                <span class="font-semibold mr-1">
+                  Region:
+                </span>
+                ${region}
+              </div>
+              <div class="mb-1">
+                <span class="font-semibold mr-1">
+                  Capital:
+                </span>
+                ${capital}
+              </div>
+            </div>
+          </a>
+        </div>`;
+      gridInner += template;
+      if (index === array.length - 1) {
+        let gridLayout = document.getElementById("grid");
+        grid.innerHTML = gridInner;
+        let popoverTriggerList = [].slice.call(
+          document.querySelectorAll('[data-toggle="popover"]')
+        );
+        let popoverList = popoverTriggerList.forEach((element) => {
+          let content = element.getAttribute("data-content");
+          tippy(element, {
+            content: content,
+            arrow: true,
+            placement: "bottom-start",
+          });
+        });
+      }
+    });
+  }
+}
+
+
+function createGridSkeleton() {
+  let gridElement = document.getElementById("grid");
+  let skeleton = `
+    <div class="mb-10">
+      <div class="card block bg-white text-black dark:bg-gray-800 rounded-lg shadow-lg
+        dark:text-white overflow-hidden">
+        <div class="w-full h-72 lg:h-72 xl:h-48 object-cover bg-gray-200 dark:bg-gray-500 animate-pulse">
+        </div>
+        <div class="card-body px-5 pt-5 pb-10">
+          <div class="mb-4 h-6 rounded bg-gray-200 dark:bg-gray-500 animate-pulse" aria-hidden="true">
+          </div>
+          <div class="mb-1 h-4 w-1/2 rounded bg-gray-200 dark:bg-gray-500 animate-pulse">
+          </div>
+          <div class="mb-1 h-4 w-1/2 rounded bg-gray-200 dark:bg-gray-500 animate-pulse">
+          </div>
+          <div class="mb-1 h-4 w-1/2 rounded bg-gray-200 dark:bg-gray-500 animate-pulse">
+          </div>
+        </div>
+      </div>
+    </div>`;
+  gridElement.innerHTML = "";
+  for (let i = 0; i < 8; i++) {
+    gridElement.innerHTML += skeleton;
+  }
+}
+
+function getAllCountries() {
+  createGridSkeleton();
+  fetch("https://restcountries.com/v3.1/all?fields=name,cca3,region,population,capital,flags")
+    .then((response) => response.json())
+    .then((data) => {
+      showCountries(data);
+      showingAllCountries = true;
+    })
+    .catch((error) => console.error(error));
+}
+
+getAllCountries();
