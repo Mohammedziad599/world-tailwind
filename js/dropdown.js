9d9708cc08dd546ac9c6d0e0678b1d18d953b36e
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-07-06 11:06:07 +0300

    update the dropdown js
    make the dropdown hide when clicked outside it
    and when clicked on one of it's element

--- js/dropdown.js
+++ js/dropdown.js
@@ -3,10 +3,17 @@ const dropdown_buttons = document.querySelectorAll("*[data-dropdown-toggle]");
 function toggleDropdown(event) {
   let dropdownId = event.currentTarget.getAttribute("data-dropdown-toggle");
   let dropdown = document.getElementById(dropdownId);
-  if (dropdown)
-    dropdown.classList.toggle("hidden");
+  if (dropdown) dropdown.classList.toggle("hidden");
+}
+function offDropdown(event) {
+  let dropdownId = event.currentTarget.getAttribute("data-dropdown-toggle");
+  setTimeout(() => {
+    let dropdown = document.getElementById(dropdownId);
+    if (dropdown) dropdown.classList.add("hidden");
+  }, 200);
 }
 
-dropdown_buttons.forEach(button => {
+dropdown_buttons.forEach((button) => {
   button.onclick = toggleDropdown;
+  button.onblur = offDropdown;
 });
